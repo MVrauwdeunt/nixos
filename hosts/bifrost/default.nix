065a8ce61a841da8e34e62e695afc4f46c1e444a
Michel <zanbee@gmail.com>
2025-08-14 07:30:03 +0200

    wip: current working config

--- hosts/bifrost/default.nix
+++ hosts/bifrost/default.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ lib, ... }:
 {
   imports = [ ./hardware-configuration.nix ];
   # networking.hostName = "Bifrost";
+
+  networking.useNetworkd = lib.mkForce false;
+  systemd.network.enable = lib.mkForce false;
+  networking.useDHCP = lib.mkForce true;
 
   # Pas aan als je NVMe hebt:
   boot.loader.grub = {
