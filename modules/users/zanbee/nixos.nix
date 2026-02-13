69fdbe2c4eb42ff827ff206f02ca3062f736e978
Michel Vrauwdeunt <michel.vrauwdeunt@iwell.eu>
2026-02-13 07:17:46 +0100

    Added ZSH

--- modules/users/zanbee/nixos.nix
+++ modules/users/zanbee/nixos.nix
@@ -3,6 +3,7 @@
   users.users.zanbee = {
     isNormalUser = true;
     description = "Zanbee";
+    shell = pkgs.zsh;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRs/wq/6uI7umQNWzFC11sxBfeK8ny4ZAq02a+DX2Cv"
