c6e0d6deaf7f3eae83baa27ab3ae8391cf229217
Michel Vrauwdeunt <michel.vrauwdeunt@iwell.eu>
2026-02-14 10:11:23 +0100

    Added zsh per role

--- modules/users/zanbee/nixos.nix
+++ modules/users/zanbee/nixos.nix
@@ -1,12 +1,23 @@
-{ pkgs, inputs, ... }:
+{ pkgs, inputs, lib, config, ... }:
+
+let
+  cfg = config.my.users.zanbee;
+in
 {
-  users.users.zanbee = {
+  options.my.users.zanbee.shell = lib.mkOption {
+    type = lib.types.package;
+    default = pkgs.bashInteractive; # servers = safe default
+    description = "Login shell for user zanbee";
+  };
+
+  config.users.users.zanbee = {
     isNormalUser = true;
     description = "Zanbee";
-    shell = pkgs.zsh;
+    shell = cfg.shell;
     extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRs/wq/6uI7umQNWzFC11sxBfeK8ny4ZAq02a+DX2Cv"
     ];
   };
 }
+
