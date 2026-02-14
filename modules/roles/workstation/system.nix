94391b99d59f426f651707f433e080be69e60fd4
Michel Vrauwdeunt <michel.vrauwdeunt@iwell.eu>
2026-02-14 11:01:32 +0100

    Repaird audio?

--- modules/roles/workstation/system.nix
+++ modules/roles/workstation/system.nix
@@ -8,7 +8,14 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
-  sound.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+  security.rtkit.enable = true;
+
   hardware.pulseaudio.enable = false;
 
   services.printing.enable = true;
