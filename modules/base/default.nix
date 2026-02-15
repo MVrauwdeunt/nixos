c5760f62ceecd064dadc4ae2af875b4929e27daa
Michel Vrauwdeunt <michel.vrauwdeunt@iwell.eu>
2026-02-15 01:08:01 +0100

    move packages

---
+++ modules/base/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # VM-friendly
+  services.qemuGuest.enable = true;
+  boot.initrd.availableKernelModules = [ "virtio_pci" "virtio_scsi" "virtio_blk" "virtio_net" ];
+
+  time.timeZone = "Europe/Amsterdam";
+  networking.firewall.enable = true;
+
+  # QoL
+  environment.systemPackages = [ pkgs.kitty.terminfo ];
+  boot.tmp.cleanOnBoot = true;
+  zramSwap.enable = true;
+
+  # SSH (details hardening in aparte module)
+  services.openssh.enable = true;
+
+  # NIET zomaar verhogen op bestaande hosts
+  system.stateVersion = "25.05";
+  imports = [
+    ./modules/packages.nix
+  ];
+}
