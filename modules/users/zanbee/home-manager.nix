8461e90102b1d7d439cecb01fcb855eaf5d06ee7
Michel Vrauwdeunt <michel.vrauwdeunt@iwell.eu>
2026-02-15 14:23:19 +0100

    Enable home

---
+++ modules/users/zanbee/home-manager.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  home.username = "zanbee";
+  home.homeDirectory = "/home/zanbee";
+  home.stateVersion = "25.05";
+
+  programs.home-manager.enable = true;
+
+  programs.bash.enable = true;
+
+  programs.starship.enable = true;
+  programs.kitty.enable = true;
+
+  programs.fzf = {
+    enable = true;
+    enableBashIntegration = true;
+  };
+
+  home.packages = with pkgs; [
+    ripgrep
+    fd
+    jq
+  ];
+}
+
