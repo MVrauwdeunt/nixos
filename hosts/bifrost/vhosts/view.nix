ddc4092cd9c053f3e922b585250dcf2e426be853
MVrauwdeunt <github@openmailbox.nl>
2026-03-29 16:13:14 +0200

    Added live view proxy to Bifrost

--- hosts/bifrost/vhosts/view.nix
+++ hosts/bifrost/vhosts/view.nix
@@ -1,14 +1,13 @@
 { lib, ... }:
 {
   apps.caddy.virtualHosts."view.gladsheimr.nl" = {
+    upstream = "http://127.0.0.1:8889";
+
     extraConfig = ''
       encode gzip
 
       # Stuur root direct naar de drone-feed
       redir / /drone
-
-      # Proxy alles naar MediaMTX WebRTC HTTP endpoint
-      reverse_proxy 127.0.0.1:8889
 
       header {
         Strict-Transport-Security "max-age=31536000; includeSubDomains; preload"
