d929e6abaf23e820fad64dff37643ca273a1162d
MVrauwdeunt <github@openmailbox.nl>
2026-04-21 23:06:22 +0200

    Added config to Renovate

--- hosts/sif/renovate.nix
+++ hosts/sif/renovate.nix
@@ -2,6 +2,8 @@
 {
   apps.renovate = {
     enable = true;
+    tokenEnvFile = "/run/secrets/renovate-env";
+    configFile = "/run/secrets/renovate-config.js";
     endpoint = "https://forgejo.fiordland-gar.ts.net/api/v1/";
     repositories = [ "zanbee/nixos" ];
     logLevel = "info";
--- hosts/sif/sops.nix
+++ hosts/sif/sops.nix
@@ -3,4 +3,14 @@
   sops.secrets."sif/tailscale" = {
     sopsFile = ../../secrets.yaml;
   };
+  
+  sops.secrets."sif/renovate-env" = {
+    sopsFile = ../../secrets.yaml;
+    path = "/run/secrets/renovate-env";
+  };
+
+  sops.secrets."sif/renovate-config" = {
+    sopsFile = ../../secrets.yaml;
+    path = "/run/secrets/renovate-config.js";
+  };  
 }
