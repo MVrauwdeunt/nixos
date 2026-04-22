df2f7197192107d1429418a54f446f4adf9f067d
MVrauwdeunt <github@openmailbox.nl>
2026-04-22 19:44:41 +0200

    Renovete again.

--- hosts/sif/renovate.nix
+++ hosts/sif/renovate.nix
@@ -4,7 +4,7 @@
     enable = true;
     tokenEnvFile = "/run/secrets/renovate-env";
     configFile = "/run/secrets/renovate-config.js";
-    endpoint = "http://192.168.178.150/api/v1/";
+    endpoint = "http://192.168.100.150/api/v1/";
     repositories = [ "zanbee/nixos" ];
     logLevel = "info";
   };
