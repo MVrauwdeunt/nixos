be7f94b58efe92a1400cee018cde3e93905a0a91
MVrauwdeunt <github@openmailbox.nl>
2026-04-22 19:13:29 +0200

    Renovete again.

--- hosts/sif/renovate.nix
+++ hosts/sif/renovate.nix
@@ -4,7 +4,7 @@
     enable = true;
     tokenEnvFile = "/run/secrets/renovate-env";
     configFile = "/run/secrets/renovate-config.js";
-    endpoint = "https://forgejo.fiordland-gar.ts.net/api/v1/";
+    endpoint = "http://127.0.0.1/api/v1/";
     repositories = [ "zanbee/nixos" ];
     logLevel = "info";
   };
