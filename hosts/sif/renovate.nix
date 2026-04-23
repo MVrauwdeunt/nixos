7739bde93b875576158001f8d2b886fff84780da
MVrauwdeunt <github@openmailbox.nl>
2026-04-23 16:48:34 +0200

    Remove debug and non needed stuff

--- hosts/sif/renovate.nix
+++ hosts/sif/renovate.nix
@@ -3,8 +3,6 @@
   apps.renovate = {
     enable = true;
     tokenEnvFile = "/run/secrets/renovate-env";
-    configFile = "/run/secrets/renovate-config.js";
-    endpoint = "http://192.168.100.150/api/v1/";
     repositories = [ "zanbee/nixos" ];
     logLevel = "info";
   };
