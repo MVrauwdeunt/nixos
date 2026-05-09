e1625f8626cabf790216c7b0c556b6fb50977dab
MVrauwdeunt <github@openmailbox.nl>
2026-05-09 23:06:13 +0200

    Added Profilarr

--- modules/containers/profilarr.nix
+++ modules/containers/profilarr.nix
@@ -1,61 +1,23 @@
-{ config, lib, ... }:
+virtualisation.oci-containers.containers.profilarr = {
+  image = "docker.io/santiagosayshey/profilarr:latest";
 
-let
-  cfg = config.apps.profilarr;
+  ports = [
+    "127.0.0.1:${toString cfg.port}:6868"
+  ];
 
-in
-{
-  options.apps.profilarr = {
-    enable = lib.mkEnableOption "Profilarr container";
+  volumes = [
+    "${cfg.dataDir}:/config"
+  ];
 
-    dataDir = lib.mkOption {
-      type = lib.types.str;
-      default = "/var/lib/profilarr";
-    };
-
-    port = lib.mkOption {
-      type = lib.types.port;
-      default = 6868;
-    };
-
-    openFirewall = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
+  environment = {
+    PUID = "1000";
+    PGID = "1000";
+    UMASK = "022";
+    TZ = "Europe/Amsterdam";
   };
 
-  config = lib.mkIf cfg.enable {
-    virtualisation.oci-containers.containers.profilarr = {
-      image = "ghcr.io/dictionarry-hub/profilarr:latest";
-
-      ports = [ "${toString cfg.port}:6868" ];
-
-      volumes = [
-        "${cfg.dataDir}:/config"
-      ];
-
-      environment = {
-        TZ = "Europe/Amsterdam";
-        PUID = "1000";
-        PGID = "1000";
-        UMASK = "022";
-        PORT = "6868";
-        HOST = "0.0.0.0";
-        AUTH = "on";
-      };
-
-      extraOptions = [
-        "--network=bridge"
-        "--health-cmd=none"
-      ];
-    };
-
-    systemd.tmpfiles.rules = [
-      "d ${cfg.dataDir} 0755 1000 1000 -"
-    ];
-
-    networking.firewall = lib.mkIf cfg.openFirewall {
-      allowedTCPPorts = [ cfg.port ];
-    };
-  };
-}
+  extraOptions = [
+    "--network=bridge"
+    "--health-cmd=none"
+  ];
+};
