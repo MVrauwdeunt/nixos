a9a12ba732a28f046e78dfbded8ef3145968d829
MVrauwdeunt <github@openmailbox.nl>
2026-05-10 16:27:27 +0200

    Cleanup

--- hosts/mimir/containers/default.nix
+++ hosts/mimir/containers/default.nix
@@ -1,28 +1,25 @@
-{ ... }:
+{ lib, ... }:
 
 let
-  containers = [
-    "jellyfin"
-    "seerr"
-    "prowlarr"
-    "radarr"
-    "sonarr"
-    "bazarr"
-    "lidarr"
-    "sabnzbd"
-    "soularr"
-    "slskd"
-    "lidify"
-    "profilarr"
-    "newtarr"
-  ];
-  
+  dir = ./.;
+
+  files = builtins.attrNames (builtins.readDir dir);
+
+  containerFiles =
+    builtins.filter
+      (name:
+        name != "default.nix"
+        && lib.hasSuffix ".nix" name)
+      files;
+
+  containerNames =
+    map (name: lib.removeSuffix ".nix" name) containerFiles;
+
   mkContainerImports = name: [
     ../../../modules/containers/${name}.nix
     ./${name}.nix
   ];
-
 in
 {
-  imports = builtins.concatLists (map mkContainerImports containers);
+  imports = builtins.concatLists (map mkContainerImports containerNames);
 }
