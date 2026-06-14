9c2c7f5d6c463e1ca8e6aaf03ab54ecbaa332919
MVrauwdeunt <github@openmailbox.nl>
2026-06-14 09:48:07 +0200

    added Hermes

--- modules/hermes-agent.nix
+++ modules/hermes-agent.nix
@@ -5,8 +5,28 @@
     inputs.hermes-agent.nixosModules.default
   ];
 
+  virtualisation.docker.enable = false;
+  virtualisation.podman.enable = true;
+  virtualisation.oci-containers.backend = "podman";
+
+  environment.systemPackages = [
+    pkgs.podman
+    config.services.hermes-agent.package
+
+    (pkgs.writeShellScriptBin "hermes-container" ''
+      exec ${pkgs.podman}/bin/podman exec -it hermes-agent \
+        env HERMES_HOME=/data/.hermes HOME=/home/hermes \
+        /data/current-package/bin/hermes "$@"
+    '')
+  ];
+
   services.hermes-agent = {
-    enable = false;
+    enable = true;
+
+    container = {
+      enable = true;
+      backend = "podman";
+    };
 
     environmentFiles = [
       config.sops.secrets."vili/hermes/env".path
@@ -18,7 +38,5 @@
         default = "gpt-5.1";
       };
     };
-
-    addToSystemPackages = true;
   };
 }
