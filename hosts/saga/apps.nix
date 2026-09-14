256babdc1e78e3c44fb34e57f7df2d4a85bc27f1
MVrauwdeunt <github@openmailbox.nl>
2026-09-14 22:04:09 +0200

    Add Spoolman to Saga

--- hosts/saga/apps.nix
+++ hosts/saga/apps.nix
@@ -8,6 +8,7 @@ let
     "dawarich"
     "wanderer"
     "filebrowser"
+    "spoolman"
   ];
 
   appModules =
---
+++ modules/containers/spoolman.nix
@@ -0,0 +1,53 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.apps.spoolman;
+in
+{
+  options.apps.spoolman = {
+    enable = lib.mkEnableOption "Spoolman";
+
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 7912;
+      description = "Local Spoolman web port.";
+    };
+
+    openFirewall = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+    };
+
+    tailscale.enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Expose Spoolman through Tailscale Services.";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.tmpfiles.rules = [
+      "d /var/lib/spoolman 0755 - - -"
+    ];
+
+    virtualisation.oci-containers.containers.spoolman = {
+      image = "ghcr.io/donkie/spoolman:latest";
+
+      ports = [
+        "127.0.0.1:${toString cfg.port}:8000"
+      ];
+
+      volumes = [
+        "/var/lib/spoolman:/home/app/.local/share/spoolman"
+      ];
+
+      environment = {
+        TZ = "Europe/Amsterdam";
+      };
+    };
+
+    networking.firewall = lib.mkIf cfg.openFirewall {
+      allowedTCPPorts = [ cfg.port ];
+    };
+  };
+}
