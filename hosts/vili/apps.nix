b5f2251621eac228e6e827e5b40c12eeb3bf0abe
MVrauwdeunt <github@openmailbox.nl>
2026-09-15 19:39:33 +0200

    Add Open-Webui to Vili

--- hosts/vili/apps.nix
+++ hosts/vili/apps.nix
@@ -4,17 +4,28 @@ let
   enabledModules = [
     "hermes-agent"
     "signal-cli"
+  ];
+
+  enabledApps = [
     "open-webui"
   ];
 
   modules =
     map (name: ../../modules/${name}.nix) enabledModules;
+
+  appModules =
+    map (name: ../../modules/containers/${name}.nix) enabledApps;
 in
 {
-  imports = modules;
+  imports = modules ++ appModules;
 
   services.signalCli = {
     enable = true;
     httpPort = 8080;
   };
+
+  apps = lib.genAttrs enabledApps (_: {
+    enable = true;
+    openFirewall = false;
+  });
 }
