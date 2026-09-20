bdbcff69eea3a30581833497e11d8c45aa53e826
MVrauwdeunt <github@openmailbox.nl>
2026-09-21 00:02:11 +0200

    flake.lock updated and fix Signal

--- modules/signal-cli.nix
+++ modules/signal-cli.nix
@@ -2,15 +2,7 @@
 
 let
   cfg = config.services.signalCli;
-
-  signalCliPkg = pkgs.signal-cli.overrideAttrs (old: rec {
-    version = "0.14.4.1";
-
-    src = pkgs.fetchzip {
-      url = "https://github.com/AsamK/signal-cli/releases/download/v${version}/signal-cli-${version}.tar.gz";
-      hash = "sha256-o8CdB4kTprSNkuT7TwpFLsqtDlaWepMVcG0vQdwJNFQ=";
-    };
-  });
+  signalCliPkg = pkgs.signal-cli;
 in
 {
   options.services.signalCli = {
