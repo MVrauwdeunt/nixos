93694dd18d513b8c674e14ab27ea272c22750e4e
MVrauwdeunt <github@openmailbox.nl>
2026-09-20 16:51:39 +0200

    Do nit restart tailscale during NixOS switches

--- modules/tailscale.nix
+++ modules/tailscale.nix
@@ -1,17 +1,23 @@
 # modules/tailscale.nix
 { config, lib, ... }:
+
 {
   services.tailscale = {
     enable = lib.mkDefault true;
     authKeyFile = config.sops.secrets.tailscaleAuthKey.path;
+
     extraUpFlags = lib.mkDefault [
       "--advertise-exit-node"
-      "--advertise-tags=tag:nixos"
       "--ssh"
     ];
+
     useRoutingFeatures = lib.mkDefault "server";
   };
 
-  # Poort voor TS
+  # Keep Tailscale running during NixOS switches so remote
+  # deployments over the tailnet do not terminate themselves.
+  systemd.services.tailscaled.restartIfChanged = false;
+
+  # Allow direct Tailscale UDP traffic.
   networking.firewall.allowedUDPPorts = lib.mkDefault [ 41641 ];
 }
