495576c20f6b09e49170bd33932b41018b7429a3
MVrauwdeunt <github@openmailbox.nl>
2026-09-21 20:58:10 +0200

    Replaced signal-cli

--- modules/signal-cli.nix
+++ modules/signal-cli.nix
@@ -36,5 +36,30 @@ in
     systemd.tmpfiles.rules = [
       "d ${cfg.dataDir} 0750 signal-cli signal-cli -"
     ];
+
+    systemd.services.signal-cli = {
+      description = "Signal CLI HTTP daemon";
+
+      wantedBy = [ "multi-user.target" ];
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+
+      serviceConfig = {
+        Type = "simple";
+        User = "signal-cli";
+        Group = "signal-cli";
+        WorkingDirectory = cfg.dataDir;
+
+        ExecStart = ''
+          ${signalCliPkg}/bin/signal-cli \
+            --config ${cfg.dataDir} \
+            daemon \
+            --http 127.0.0.1:${toString cfg.httpPort}
+        '';
+
+        Restart = "on-failure";
+        RestartSec = "5s";
+      };
+    };
   };
 }
